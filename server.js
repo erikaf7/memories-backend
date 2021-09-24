77e9426d63bcf66e1c11034acbfda032a12ead71
Erika Fomich <erikafomich@gmail.com>
2021-09-24 13:38:03 -0700

    added books controller

--- server.js
+++ server.js
@@ -10,31 +10,6 @@ const PORT = process.env.PORT || 4000;
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongo not running?'));
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-<<<<<<< HEAD
-
-mongoose.connect('mongodb://localhost:27017/bookmarks', { useNewUrlParser: true})
-mongoose.connection.once('open', () => {
-    console.log('connected to mongoose!')
-  })
-/*
-Mongoose conection code here, waiting on the set up of the database
-*/
-
-//importing schema
-const Book = require('./models/Book')
-
-//cors
-const whitelist = ['http://localhost:3000']
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
-=======
 mongoose.connect('mongodb://localhost:27017/scrapbook', { useNewUrlParser: true });
 mongoose.connection.once('open', () => {
   console.log('connected to mongoose!!')
@@ -54,7 +29,6 @@ mongoose.connection.once('open', () => {
 // }
 
 // app.use(cors(corsOptions)) 
->>>>>>> 06b93a8138ff5ed9fe2a601a06071a8f8cbb12c8
 
 app.use(cors())
 
