46ef8f10c0d614db62959fb477b45ebbdc064e86
Allison Moss <allisonwmoss@icloud.com>
2021-09-24 16:08:11 -0400

    Seed database and fix bugs with model and server.js.

--- server.js
+++ server.js
@@ -1,52 +1,63 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const books = require('./controllers/books');
+const Book = require('./models/Book')
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // mongoose configuration
 mongoose.connection.on('error', err => console.log(err.message + ' is Mongo not running?'));
 mongoose.connection.on('disconnected', () => console.log('mongo disconnected'));
 
-/*
-Mongoose conection code here, waiting on the set up of the database
-*/
+mongoose.connect('mongodb://localhost:27017/scrapbook', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('connected to mongoose!!')
+});
 
+//**I had to temporarily comment out these CORS settings--I don't think the configuration is correct.  I could not run any GET requests with it like this. -Alli */
 
-//corsconst whitelist = ['http://localhost:3000']
-const corsOptions = {
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        callback(null, true)
-      } else {
-        callback(new Error('Not allowed by CORS'))
-      }
-    }
-  }
+// const whitelist = ['http://localhost:4000']
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !== -1) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'))
+//     }
+//   }
+// }
 
-  app.use(cors(corsOptions)) // all routes are now exposed, sometimes you just want to limit access (ie OMDB - it's ok for anyone to see the movies, but you don't want just anyone updating the movies)
+// app.use(cors(corsOptions)) 
 
+app.use(cors())
+
+//import seeds
+const seeds = require('./models/seeds')
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-
+//seed route
+app.get('/seed-data', (req, res, next) => {
+  Book.insertMany(seeds)
+    .then((res) => console.log(res))
+    .catch(next)
+  res.send('Seed entries inserted.')
 })
 
-/*
-Seed route here
-*/
+app.get('/', (req, res, next) => {
+  Book.find({})
+    .then(books => res.json(books))
+    .catch(next)
+})
 
 /*
 Controllers code here
 */
 
 
-
 app.listen(PORT, () => {
-    console.log(`✅ PORT: ${PORT} 🌟`)
+  console.log(`✅ PORT: ${PORT} 🌟`)
 })
